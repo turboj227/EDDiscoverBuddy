7eb77b0402df5d3409284cfd4d78b78c615c70f3
turboj227 <turboj@ziggo.nl>
2022-09-19 11:58:01 +0200

    Debug can't start.

--- EDDiscoverBuddy/Program.cs
+++ EDDiscoverBuddy/Program.cs
@@ -10,10 +10,10 @@ namespace EDDiscoverBuddy
         [STAThread]
         static void Main()
         {
-            if (Process.GetProcessesByName(Process.GetCurrentProcess().ProcessName).Length > 1)
+            /*if (Process.GetProcessesByName(Process.GetCurrentProcess().ProcessName).Length > 1)
             {
                 return;
-            }
+            }*/
 
             // To customize application configuration such as set high DPI settings or default font,
             // see https://aka.ms/applicationconfiguration.
